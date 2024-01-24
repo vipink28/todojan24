209242cb0f8fe5fe1470c7c94fad27a421ff233a
vipink28 <vipinkalyan28@gmail.com>
2024-01-24 11:14:41 +0530

    Auth route added.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import illustration from '../assets/illustration.png';
+import { Link, Outlet } from 'react-router-dom';
 
 function Home(props) {
     return (
-        <div>
-            Home
+        <div className='container-fluid h-100'>
+            <div className="row h-100">
+                <div className="col-lg-6 d-flex flex-column align-items-center justify-content-center h-100 bg-primary text-white">
+                    <h1 className='display-2 text-center mb-4'>An App to<br />
+                        make your life<br />
+                        easy</h1>
+                    <img className='img-fluid' src={illustration} alt="illustration" />
+                </div>
+
+                <div className="col-lg-6 d-flex flex-column align-items-center justify-content-center h-100">
+
+                    <div className='card w-50'>
+                        <div className="card-header d-flex rounded-0 p-0 text-center">
+                            <Link to="/login" className='w-50 py-2 px-3 bg-primary text-white text-decoration-none'>Login</Link>
+                            <Link to="/register" className='w-50 py-2 px-3 text-decoration-none'>Register</Link>
+                        </div>
+
+                        <div className="card-body">
+                            <Outlet />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 }
