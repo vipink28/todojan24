e69eb2ba8534395297c84575dea97048cab97c3e
vipink28 <vipinkalyan28@gmail.com>
2024-02-12 10:20:12 +0530

    format date function.

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,3 +1,8 @@
-const formatDate = (date) => {
-
+export const formatDate = (dateString) => {
+    let date = new Date(dateString);
+    return `
+    ${date.getDate()}
+    ${date.toLocaleDateString("en-US", { month: 'short' })} 
+    ${date.getFullYear()} 
+    ${date.toLocaleString("en-US", { hour: 'numeric', minute: 'numeric', hour12: true })}`
 }
